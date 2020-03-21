d2718c0f7050b4c293d2379b72a4cc6a9bf40c0e
hansol9 <hansol9@gmail.com>
2020-03-21 10:23:51 -0400

    Web Extension - Paging & Sorting

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,12 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -26,9 +29,19 @@ public class MemberController {
         return member.getUsername();
     }
 
-
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+//        memberRepository.save(new Member("userA"));
+        for (int i=0; i<100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(Pageable pageable) {
+        Page<Member> list = memberRepository.findAll(pageable);
+        Page<MemberDto> map = list.map(member -> new MemberDto(member.getId(),member.getUsername(), null));
+
+        return map;
     }
 }
